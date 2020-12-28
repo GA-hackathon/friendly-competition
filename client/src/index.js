fd189e7aafc95dac4ae230d9ffd3d7fc7388b628
Daniel Michael <itzdanielmichael@gmail.com>
2020-12-28 16:24:46 -0500

    created back-end user model and authentication, created react skeleton

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import App from './App'
-import { HashRouter } from 'react-router-dom'
-
-const appJsx = (
-  <HashRouter>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </HashRouter>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(appJsx, document.getElementById('root'))
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ client/src/services/auth.js
@@ -0,0 +1,44 @@
+import api from "./apiConfig";
+export const loginUser = async (loginData) => {
+  try {
+    const resp = await api?.post("/auth/login", { authentication: loginData });
+    localStorage.setItem("authToken", resp?.data?.token);
+    api.defaults.headers.common.authorization = `Bearer ${resp?.data?.token}`;
+    return resp?.data?.user;
+  } catch (error) {
+    alert("Email and/or password do not match");
+    throw error;
+  }
+};
+
+export const registerUser = async (registerData) => {
+  try {
+    const resp = await api.post("/users/", { user: registerData });
+    localStorage.setItem("authToken", resp.data.token);
+    api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+    return resp.data.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const verifyUser = async () => {
+  const token = localStorage.getItem("authToken");
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+    try {
+      const resp = await api.get("/auth/verify");
+      return resp.data;
+    } catch (error) {
+      let path = window.location.origin + "/login";
+      if (window.location.origin + "/login" !== path) {
+        window.location.href = window.location.origin + "/login";
+      }
+    }
+  }
+  return null;
+};
+
+export const removeToken = () => {
+  api.defaults.headers.common.authorization = null;
+};
