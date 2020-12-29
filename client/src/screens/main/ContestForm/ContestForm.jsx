16883b04d43fe5843efe8370403bf64d50d2f009
timosha <tinaatimkovaa@gmail.com>
2020-12-29 09:41:07 -0500

    handle form submission

--- client/src/screens/main/ContestForm/ContestForm.jsx
+++ client/src/screens/main/ContestForm/ContestForm.jsx
@@ -1,17 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import Input from "@material-ui/core/Input";
 import Navbar from './../Navbar/Navbar.jsx';
 import Button from '@material-ui/core/Button';
 import './ContestForm.css';
 
 function ContestForm() {
+
+    const [ formData, setFormData ] = useState({
+        contest_name: '',
+        description: '',
+        date: null,
+        zip_code: '',
+        picture: null
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormData({[e.target.name] : e.target.value })
+        console.log(formData)
+    }
+
     return (
         <>
           <Navbar />
           <div className='contest-form'>
           <h3>Creating your own contest</h3> 
           <h5>Tell us a little bit about what you'd like in the contest</h5>
-          <form className='input-group'>
+          <form className='input-group' onSubmit={handleSubmit}>
               <label>
                   Contest Name:
                   <Input type='text' name='contest_name' />
