d29bbcb5e848f9f2963abbe2ef493dc566afa5af
timosha <tinaatimkovaa@gmail.com>
2020-12-29 18:43:45 -0500

    do contest page layout

--- client/src/screens/ContestScreens/ContestPage/ContestPage.jsx
+++ client/src/screens/ContestScreens/ContestPage/ContestPage.jsx
@@ -3,6 +3,8 @@ import Search from '../../../components/Form/Search';
 import { useStateValue } from "../../../providers/CurrentUserProvider";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import './ContestPage.css'
+import Button from '@material-ui/core/Button';
+import Layout from '../../../layout/Layout'
 
 
 function ContestPage() {
@@ -46,15 +48,15 @@ function ContestPage() {
     })
 
     return (
-        <Fragment>
+        <Layout>
         <header>
           <div className='submission-name'>Best Pina Colada</div>
-          <span>Search Submissions:</span>
-          <Search />
+          <div className='search'><span style={{ 'margin-right': '1rem'}}>Search Submissions:</span>
+          <Search /></div>
         </header>
         <main>
             <section className='timer-container'>
-                <h2>Contest Ends In:</h2>
+                <h5>Contest Ends In:</h5>
             <div className='timer'>
                 <section>
                     <p>{timerDays}</p>
@@ -76,16 +78,31 @@ function ContestPage() {
                     <p><small>Seconds</small></p>
                 </section>
             </div>
-            </section>
             <h2>Contest Rules</h2>
             <div>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             Turpis dictum dui sed lacinia. Est nibh interdum lectus faucibus urna arcu nibh. 
             Nunc non sit eget dignissim. Et nulla sed non ipsum risus, quis nisi suscipit.
             </div>
+            </section>
             <div>Contest Created by: <AccountCircleIcon className="icon" /><span>{currentUser?.first_name}</span><span>{currentUser?.last_name}</span></div>
+            <section className='create-submission'>
+                <h3>Ready to Enter?</h3>
+                <form>
+                <label>Entry Name
+                <input type='text' name='submission_name'/>
+                </label>
+                <label>Write Entry
+                <input type='textarea'name='content'/>
+                </label>
+                <label>Upload Entry
+                <img src='' alt='upload-icon'/>
+                </label>
+                <Button type='submit' variant="contained">Enter Contest</Button>
+                </form>
+            </section>
         </main>
-        </Fragment>
+        </Layout>
     )
 }
 
