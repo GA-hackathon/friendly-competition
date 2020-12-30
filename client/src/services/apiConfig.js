39a2f485553f5c4c7e2ba9a41ebff6b7d7918f31
Daniel Michael <itzdanielmichael@gmail.com>
2020-12-30 11:55:53 -0500

    update apiconfig for deployment

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "heroku link here" //heroku link here once we deploy
+    ? "https://challenge-me-api.herokuapp.com/" //heroku link here once we deploy
     : "http://localhost:3000";
 
 const api = axios.create({
