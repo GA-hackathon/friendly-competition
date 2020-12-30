143df9d5525a8ed242552386a3d3e6db36f843e3
Daniel Michael <itzdanielmichael@gmail.com>
2020-12-29 19:10:06 -0500

    update home.jsx

--- client/src/components/ContestComponents/ContestCard/ContestCard.jsx
+++ client/src/components/ContestComponents/ContestCard/ContestCard.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import Moment from "react-moment";
+import "moment-timezone";
+import styled from "styled-components";
 
+let Container = styled.div`
+  picture {
+    display: block;
+  }
+
+  picture img {
+    display: block;
+    width: 100%;
+    height: auto;
+    margin: 0 auto;
+  }
+
+  .thumbnail {
+    max-width: 300px;
+    margin-top: $pad;
+    margin: 0 auto;
+    transform: scale(1.1);
+    transition: 3s;
+    &:hover {
+      transform: scale(1);
+      transition: 0.2s;
+    }
+  }
+`;
 function ContestCard({ contest }) {
-  return <img src={contest.picture} alt={contest.name}></img>;
+  return (
+    <Container>
+      <picture>
+        <img className="thumbnail" src={contest.picture} alt={contest.name} />
+      </picture>
+    </Container>
+  );
 }
 
 export default ContestCard;
--- client/src/screens/main/Home/Home.jsx
+++ client/src/screens/main/Home/Home.jsx
@@ -32,6 +32,7 @@ function Home() {
     fetchContests();
   }, []);
 
+  // using rails api to get the oldest 6, but to be honest, you can just use the slice method...
   useEffect(() => {
     const fetchOldestContests = async () => {
       const oldContestData = await getOldestContests();
@@ -45,9 +46,22 @@ function Home() {
     .slice(0, 6)
     .map((contest) => <ContestCard key={contest.id} contest={contest} />);
 
+  // only get the 6 ending soon
   const oldContestsJSX = oldestContests.map((contest) => (
     <ContestCard key={contest.id} contest={contest} />
   ));
+
+  // const oldContestsJSX = allContests
+  //   .slice(1)
+  //   .slice(-6)
+  //   .map((contest) => <ContestCard key={contest.id} contest={contest} />);
+
+  //   var array = [1, 55, 77, 88, 76, 59];
+  // var array_last_five;
+  // array_last_five = array.slice(-5);
+  // if (array.length < 6) {
+  //      array_last_five.shift();
+  // }
 
   return (
     <Layout>
@@ -59,10 +73,11 @@ function Home() {
         {!loaded ? (
           <FunOrangeLoading />
         ) : (
-          <div className="all-contests">
+          <div className="all-contests inner-column">
+            <h1> Ending Soon</h1>
             <div className="contest-container oldest">{oldContestsJSX}</div>
+            <h1> NEW Contests</h1>
             <div className="contests-container newest">
-              <h1> NEW Contests</h1>
               <div className="contests">{newContestsJSX}</div>
             </div>
           </div>
