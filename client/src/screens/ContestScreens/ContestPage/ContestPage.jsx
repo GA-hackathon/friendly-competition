c6c4032da2155514d695eeca213a7e68670ea4d4
timosha <tinaatimkovaa@gmail.com>
2020-12-29 21:31:22 -0500

    contest page layout half-done

--- client/src/screens/ContestScreens/ContestPage/ContestPage.jsx
+++ client/src/screens/ContestScreens/ContestPage/ContestPage.jsx
@@ -1,10 +1,13 @@
 import React, { Fragment, useState, useRef, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Search from '../../../components/Form/Search';
 import { useStateValue } from "../../../providers/CurrentUserProvider";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import './ContestPage.css'
 import Button from '@material-ui/core/Button';
 import Layout from '../../../layout/Layout'
+import TextField from '@material-ui/core/TextField';
+import AddCircle from '@material-ui/icons/AddCircle';
 
 
 function ContestPage() {
@@ -78,30 +81,45 @@ function ContestPage() {
                     <p><small>Seconds</small></p>
                 </section>
             </div>
-            <h2>Contest Rules</h2>
+            <h5>Contest Rules</h5>
             <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Turpis dictum dui sed lacinia. Est nibh interdum lectus faucibus urna arcu nibh. 
-            Nunc non sit eget dignissim. Et nulla sed non ipsum risus, quis nisi suscipit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/> 
+            Turpis dictum dui sed lacinia. Est nibh interdum lectus faucibus urna arcu nibh.<br/>
+            Nunc non sit eget dignissim. Et nulla sed non ipsum risus, quis nisi suscipit.<br/>
             </div>
+            <h5>Try</h5><span></span>
+            <p>Join the Discussion</p>
             </section>
-            <div>Contest Created by: <AccountCircleIcon className="icon" /><span>{currentUser?.first_name}</span><span>{currentUser?.last_name}</span></div>
-            <section className='create-submission'>
-                <h3>Ready to Enter?</h3>
-                <form>
-                <label>Entry Name
-                <input type='text' name='submission_name'/>
+            <div className='create-submission'>Contest Created by: <AccountCircleIcon className="icon-submission" /><span>{currentUser?.first_name}</span><span>{currentUser?.last_name}</span>
+            <section>
+                <h5>Ready to Enter?</h5>
+                <form className='submissions-form'>
+                <label>Entry Name<br/>
+                <TextField variant='filled' type='text' name='submission_name'/>
                 </label>
-                <label>Write Entry
-                <input type='textarea'name='content'/>
+                <label>Write Entry<br/>
+                <TextField multiline rows={4} variant='filled' type='textarea'name='content'/>
                 </label>
                 <label>Upload Entry
-                <img src='' alt='upload-icon'/>
+                <input
+                type="file"
+                id="image-upload"
+                style={{ visibility: "hidden", fontSize: "0" }}
+                // onChange={onImageSelected}
+                />
+                <div className='add-circle-icon'>
+                <AddCircle style={{ fontSize: 30 }}></AddCircle>
+                </div>
                 </label>
-                <Button type='submit' variant="contained">Enter Contest</Button>
+                <Button className='enter-contest-btn' type='submit' variant="contained">Enter Contest</Button>
                 </form>
             </section>
+            </div>
         </main>
+        <hr style={{ margin: "0rem 2rem"}}/>
+        <section>
+        <div className='submission-name'>View Contest Entries</div>
+        </section>
         </Layout>
     )
 }
